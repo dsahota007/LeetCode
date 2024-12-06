68377c33abec801dab2b526e72543d6d44d9e5bb
Diljot Singh Sahota <104109226+dsahota007@users.noreply.github.com>
2024-12-05 16:44:55 -0800

    Time: 2009 ms (40.56%), Space: 13.2 MB (5.63%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,9 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        n = len(nums)
-        for i in range(n):
-            for j in range(i + 1, n):
+        n = len(nums)  #calc the length of the list and storing it in n 
+        for i in range(n): #traverse the entire list of n 
+            for j in range(i + 1, n): # inner loop - ensures that the same pair o fnum is not checked twice and avoid a num itself
                 if nums[i] + nums[j]==target:
                     return[i,j]
-        return None
+        return None #if we cannot find pair than return None 
         
